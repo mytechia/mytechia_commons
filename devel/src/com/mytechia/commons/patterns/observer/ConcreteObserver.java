2a79342a40fb4a1122b05c8e86b5160a4fdce0c0
amalloca <alma.mallo@udc.es>
2014-02-05 13:06:51 +0100

    New release of MyTechia Commons (v1.01):
    - Added a Maven ide project
    - Minor code changes to adapt the library for Android and JAVA 1.6

--- devel/src/com/mytechia/commons/patterns/observer/ConcreteObserver.java
+++ devel/src/com/mytechia/commons/patterns/observer/ConcreteObserver.java
@@ -1,23 +1,41 @@
+/*******************************************************************************
+ *   
+ *   Copyright 2013 Mytech Ingenieria Aplicada <http://www.mytechia.com>
+ * 
+ *   This file is part of Mytechia Commons.
+ *
+ *   Mytechia Commons is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU Lesser General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   Mytechia Commons is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Lesser General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Lesser General Public License
+ *   along with Mytechia Commons.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ ******************************************************************************/
+
 package com.mytechia.commons.patterns.observer;
 
 /**
- * Clase que factoriza el comportamiento de el sujeto Observador.
+ * An observer object
  */
 public class ConcreteObserver implements Observer {
-    /** Sujeto observado */
+    /** observer subject */
     private Subject observado;
     
     
-    /**
-     * Constructor de clase.
-     * @param observado Objeto observado relacionado con este observador.
-     */
+
     public ConcreteObserver(Subject observado){
         this.observado = observado;
     }
     
     /**
-     * Implementaci?n vac?a del m?todo actualizar
+     * This method is called when a change in the observer object is detected
      */
     public void update(Object o){
     }
